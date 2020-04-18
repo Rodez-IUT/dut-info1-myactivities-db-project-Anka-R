0672ddf989fcc7185427b8e64c5ce70ee6f00f07
Anka-R <56653321+Anka-R@users.noreply.github.com>
2020-04-18 12:01:53 +0200

    fix #6 fonction P3-2-1 fonction insert_salle

---
+++ P3-2-1-insert_salle.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION gds.insert_salle(
+        un_nom VARCHAR(200), 
+        un_nb_personne_max integer)
+    RETURNS gds.salle AS $$
+        -- insertion de la salle
+        INSERT INTO gds.salle (id, nom, nb_personnes_max, date_creation, date_modification) 
+        VALUES (nextval('gds.salle_seq'), un_nom, un_nb_personne_max, now(), now())
+        -- return de la salle créée
+		RETURNING salle;
+	$$ LANGUAGE SQL;
