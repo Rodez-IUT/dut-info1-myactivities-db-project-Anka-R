156e13187b6414226bf1b9b71790036ebbdb9c1f
Anka-R <56653321+Anka-R@users.noreply.github.com>
2020-05-04 11:33:16 +0200

    fix #13 fonction pl_calcule_temps_occupation modifiée

---
+++ P5-1-pl_calcule_temps_occupation.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION gds.pl_calcule_temps_occupation(
+	un_nom_de_salle character varying(250)) RETURNS interval AS $$
+	
+	DECLARE
+		temps_occup interval;
+	
+	BEGIN
+		SELECT * INTO temps_occup FROM gds.salle WHERE nom = un_nom_de_salle;
+		IF FOUND THEN
+			SELECT SUM(age(date_fin, date_debut)) INTO temps_occup FROM gds.reservation WHERE salle_id = (SELECT id
+																						 FROM gds.salle
+																						 WHERE nom = un_nom_de_salle);
+		ELSE
+			RAISE invalid_parameter_value
+				USING MESSAGE= 'Salle non trouvée';
+		END IF;
+		
+	RETURN temps_occup;
+	END;
+	$$ LANGUAGE plpgsql;
